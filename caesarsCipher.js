3fc149b42269c5d1b3560ab6c3feab45d9525c29
Benjamin Höfer <benjamin.hoefer@gmx.de>
2023-02-06 08:26:45 +0100

    Final commit

--- caesarsCipher.js
+++ caesarsCipher.js
@@ -1,8 +1,27 @@
 function rot13(str) {
 
-    const abc = [a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z]
+    // Variables and constants for the cipher
+    const abc = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+    const strArr = str.split("");
+    let cipherStr = "";
+    let index;
 
-    return str;
+    // Get the index of the letters of the string from the abc
+    for (let letter of strArr) {
+        if (abc.includes(letter)) {
+            if (abc[abc.indexOf(letter) + 13] !== undefined) {
+                index = abc.indexOf(letter) + 13;
+                cipherStr += abc[index];
+            }   else {
+                    index = Math.abs(26 - (abc.indexOf(letter) + 13));
+                    cipherStr += abc[index];
+            }
+        } else {
+            cipherStr += letter;
+        }
+        
+    }
+    return cipherStr;
 }
 
 rot13("SERR PBQR PNZC");
