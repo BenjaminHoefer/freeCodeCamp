2d77d8d8f5f7772dbaa1fbf26afbc93cd2bc8045
Benjamin Höfer <benjamin.hoefer@gmx.de>
2023-02-07 08:40:21 +0100

    Initial commit

--- telephoneCheck.js
+++ telephoneCheck.js
@@ -1,6 +1,31 @@
 function telephoneCheck(str) {
 
-    return true;
+    if (!/[^-() \d]/g.test(str)) {
+        
+        const digArr = str.split(/\D/g);
+        let count = 0;
+        const countryCode = 1;
+
+        for (let num of digArr) {
+            count += num.length;
+        }
+
+        switch (true) {
+            case (count > 11):
+                return false;
+            case (count < 10):
+                return false;
+            case (count === 11):
+                if (parseInt(str[0]) !== countryCode) {
+                    return false;
+                }
+        }
+        return true;
+    }
+    return false;
 }
 
-telephoneCheck("555-555-5555");
+debugger;
+console.log(telephoneCheck("55 55-55-555-5"));
+console.log(telephoneCheck("555-555-5555"));
+console.log(telephoneCheck("555)-555-5555"))
