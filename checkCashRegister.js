85f2a139676896f9a20a34d8737c10fd80372c56
Benjamin Höfer <benjamin.hoefer@gmx.de>
2023-02-19 13:14:11 +0100

    final commit

--- checkCashRegister.js
+++ checkCashRegister.js
@@ -1,14 +1,13 @@
 function checkCashRegister(price, cash, cid) {
 
-    let lengtCurr = 9;
     const units = [100, 20, 10, 5, 1, 0.25, 0.1, 0.05, 0.01]; 
     const currUnit = {"PENNY": 0.01, "NICKEL": 0.05, "DIME": 0.1, "QUARTER": 0.25, "DOLLAR": 1, "FIVE DOLLARS": 5, "TEN DOLLARS": 10, "TWENTY DOLLARS": 20, "ONE-HUNDRED DOLLARS": 100};
     const nameUnit = ["PENNY", "NICKEL", "DIME", "QUARTER", "DOLLAR", "FIVE DOLLARS", "TEN DOLLARS", "TWENTY DOLLARS", "ONE-HUNDRED DOLLARS"];
-    const amountUnit = [];
-    const accessCash = [];
 
     // Write the accessible cash for change in array
-    for (let i = 0; i < lengtCurr; i++) {
+    const accessCash = [];
+
+    for (let i = 0; i < units.length; i++) {
         accessCash.push(Math.round(cid[i][1] / currUnit[nameUnit[i]]));
     }
     accessCash.reverse();
@@ -17,31 +16,135 @@ function checkCashRegister(price, cash, cid) {
     let change = cash - price;
 
     // Check which kind of change is even possible
-    let cashBack = units.filter(unit => change / unit >= 1);
+    const cashBack = [];
 
-    // Cut accessCash to the necessary length
-    diffCurr = 9 - cashBack.length;
-    accessCash.splice(0, diffCurr);
+    for (let unit of units) {
+        if (change / unit >= 1) {
+            cashBack.push(unit);
+        } else {
+            cashBack.push(0);
+        }
+    }
 
-    // // Array which gives back the amount of coins and notes for change
-    // let amountCoins = [];
+    // Array which gives back the amount of coins and notes for change
+    const amountCoins = [];
+    let checkVar = 0;
 
-    // for (let unit of cashBack) {
-    //     if (change % unit === 0) {
-    //         amountCoins.push(change / unit );
-    //         return amountCoins;
-    //     } else {
-    //         amountCoins.push(Math.floor(change / unit ));
-    //         change -= Math.floor(change / unit ) * unit;
-    //         change = parseFloat(change.toFixed(2));
-    //     }
-    // }
+    for (let i = 0; i < cashBack.length; i++) {
+        if (cashBack[i] === 0) {
+            amountCoins.push(cashBack[i]);
+        } else if (change % cashBack[i] === 0) {
+            if (Math.floor(change / cashBack[i]) >= accessCash[i]) {
+                amountCoins.push(accessCash[i])
+                change -= accessCash[i] * cashBack[i];
+                change = parseFloat(change.toFixed(2));
+            } else {
+                amountCoins.push(Math.floor(change / cashBack[i]));
+                change -= Math.floor(change / cashBack[i] ) * cashBack[i];
+                change = parseFloat(change.toFixed(2));
+                checkVar = 1;
+                }
+        } else {
+            if (Math.floor(change / cashBack[i]) >= accessCash[i]) {
+                amountCoins.push(accessCash[i])
+                change -= accessCash[i] * cashBack[i];
+                change = parseFloat(change.toFixed(2));
 
-    return accessCash;
+            } else {
+                amountCoins.push(Math.floor(change / cashBack[i]));
+                change -= Math.floor(change / cashBack[i] ) * cashBack[i];
+                change = parseFloat(change.toFixed(2));
+                checkVar = 1;
+            }
+        }
+    }
+
+    // Determine the status of the cash checkCashRegister
+    let status = "";
+
+    if (change === 0 && checkVar === 1) {
+        status = "OPEN";
+    } else if (change === 0 && checkVar === 0) {
+        status = "CLOSED";
+    } else {
+        status = "INSUFFICIENT_FUNDS"
+    }
+
+    // Create the desired ouput format
+    let output = [];
+    let partialOutput = [];
+
+    for (let index = 0; index < amountCoins.length; index++) {
+        if (status === "OPEN" || status === "CLOSED") {
+            if (amountCoins[index] === 0 && status === "OPEN") {
+                continue;
+            } else {
+                switch (index) {
+                    case 0:
+                        partialOutput.push("ONE HUNDRED");
+                        partialOutput.push(amountCoins[index] * 100);
+                        output.push(partialOutput);
+                        partialOutput = [];
+                        break;
+                    case 1:
+                        partialOutput.push("TWENTY");
+                        partialOutput.push(amountCoins[index] * 20);
+                        output.push(partialOutput);
+                        partialOutput = [];
+                        break;
+                    case 2:
+                        partialOutput.push("TEN");
+                        partialOutput.push(amountCoins[index] * 10);
+                        output.push(partialOutput);
+                        partialOutput = [];
+                        break;
+                    case 3:
+                        partialOutput.push("FIVE");
+                        partialOutput.push(amountCoins[index] * 5);
+                        output.push(partialOutput);
+                        partialOutput = [];
+                        break;
+                    case 4:
+                        partialOutput.push("ONE");
+                        partialOutput.push(amountCoins[index] * 1);
+                        output.push(partialOutput);
+                        partialOutput = [];
+                        break;
+                    case 5:
+                        partialOutput.push("QUARTER");
+                        partialOutput.push(amountCoins[index] * 0.25);
+                        output.push(partialOutput);
+                        partialOutput = [];
+                        break;
+                    case 6:
+                        partialOutput.push("DIME");
+                        partialOutput.push(amountCoins[index] * 0.1);
+                        output.push(partialOutput);
+                        partialOutput = [];
+                        break;
+                    case 7:
+                        partialOutput.push("NICKEL");
+                        partialOutput.push(amountCoins[index] * 0.05);
+                        output.push(partialOutput);
+                        partialOutput = [];
+                        break;
+                    case 8:
+                        partialOutput.push("PENNY");
+                        partialOutput.push(amountCoins[index] * 0.01);
+                        output.push(partialOutput);
+                        partialOutput = [];
+                }   
+            }
+        }
+    }
+
+    if (status === "CLOSED") {
+        output.reverse();
+    }
+    
+    let finalChange = {status: status, change: output};
+
+    return finalChange;
 }
 
-debugger;
-// console.log(checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]));
-// console.log(checkCashRegister(19.5, 20, [["PENNY", 0.01], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]));
-console.log(checkCashRegister(3.26, 100, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]));
-// console.log(checkCashRegister(19.5, 20, [["PENNY", 0.5], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]));
+console.log(checkCashRegister(19.5, 20, [["PENNY", 0.5], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]));
